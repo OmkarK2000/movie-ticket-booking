96688c2d1e8b2ed7dfcdc83f752edf55dffceb93
OmkarK2000 <omkarkardile2000@gmail.com>
2024-02-02 00:23:15 +0530

    fourth commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -4,49 +4,69 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import parse from 'html-react-parser';
 const MovieDetails = ({ movieList }) => {
   const { id } = useParams();
-  const [form, setForm] = useState([{
+  
+const [showDetails, setShowDetails] = useState({})
+
+  const [form, setForm] = useState({
     title : "",
     email : "",
     number : ""
-}])
-  const selectedMovie = movieList.find((movie) => movie.show.id == id);
-  console.log(selectedMovie?.show);
-  setForm([...form , {title : selectedMovie?.show.name}])
+})
 
   const handleChange = (e) => {
     const {value, name} = e.target;
     setForm(
-      [...form, {[name] : value} ]
+      {
+        ...form,
+        [name]:value
+      }
     )
-    console.log(form)
   }
 
-  const handleClick = (name) => {
+const handleClick=()=>{
+  localStorage.setItem('bookingDetails',JSON.stringify(form))
+  setForm({
+    title:showDetails.name,
+    email:'',
+    number:''
+  })
+}
 
-  }
+  useEffect(()=>{
+    const selectedMovie = (movieList.find((movie) => movie.show.id == id));
+    setShowDetails({...selectedMovie?.show,summary:parse(`${selectedMovie?.show?.summary}`)})
+    setForm({
+      ...form,
+      title:selectedMovie?.show?.name
+    })
 
+  },[movieList])
+
+
+// console.log(showDetails)
   return (
     <Container className="d-flex justify-content-center align-items-center flex-column">
+      
       <Card style={{ width: '52rem' }} className="mt-4 d-flex flex-row">
         <div>
-          <Card.Img style={{ width: '16rem' }} variant="top" src={selectedMovie?.show?.image?.original} />
+          <Card.Img style={{ width: '16rem' }} variant="top" src={showDetails?.image?.original} />
         </div>
         <div>
           <Card.Body>
-            <Card.Title>{selectedMovie?.show.name}</Card.Title>
+            <Card.Title>{showDetails?.name}</Card.Title>
             <ListGroup className="list-group-flush">
-              <ListGroup.Item>Genres : {selectedMovie?.show?.genres}</ListGroup.Item>
-              <ListGroup.Item>Language : {selectedMovie?.show?.language}</ListGroup.Item>
-              <ListGroup.Item>rating : {selectedMovie?.show?.rating?.average}</ListGroup.Item>
+              <ListGroup.Item>Genres : {showDetails?.genres}</ListGroup.Item>
+              <ListGroup.Item>Language : {showDetails?.language}</ListGroup.Item>
+              <ListGroup.Item>rating : {showDetails?.rating?.average}</ListGroup.Item>
             </ListGroup>
             <hr />
             <Card.Text>
               <b>Summary</b> <br />
-              {selectedMovie?.show?.summary}
+              {showDetails?.summary}
             </Card.Text>
           </Card.Body>
         </div>
@@ -56,7 +76,7 @@ const MovieDetails = ({ movieList }) => {
         <h4>Fill form to book ticket</h4>
           <Form.Group className="mb-3" controlId="formGroupEmail">
             <Form.Label>Movie name</Form.Label>
-            <Form.Control type="text" name='title' readOnly  value={form.name} onChange={handleChange}/>
+            <Form.Control type="text" name='title' readOnly  value={form.title} onChange={handleChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupEmail">
             <Form.Label>Email address</Form.Label>
@@ -64,10 +84,10 @@ const MovieDetails = ({ movieList }) => {
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupEmail">
             <Form.Label>Mobile number</Form.Label>
-            <Form.Control type="number" name="number" placeholder="Enter mobile number" value={form.number} onChange={handleChange}/>
+            <Form.Control type="text" name="number" placeholder="Enter mobile number" value={form.number} onChange={handleChange}/>
           </Form.Group>
-          <Button variant="outline-primary">Book ticket</Button>
-        </Form>
+          <Button variant="outline-primary" onClick={handleClick}>Book ticket</Button>
+      </Form>
     </Container>
   )
 };
