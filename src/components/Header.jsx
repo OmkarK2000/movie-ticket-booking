74280f234e92e3fe9c6f2b7d64886585041bbb3b
OmkarK2000 <omkarkardile2000@gmail.com>
2024-02-02 01:13:51 +0530

    fifth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand to="/">Book my Ticket</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Link to='/' className='text-decoration-none text-dark'>Home</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
